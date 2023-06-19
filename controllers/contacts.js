b5a377bc68caaf6e5639358394a5c08a09b5fc1d
Vladyslav Simak <simakv21@gmail.com>
2023-06-19 18:55:23 +0300

    Fix Put

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -60,6 +60,11 @@ const updateContact = async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const result = await contacts.updateContact(contactId, req.body);
+
+    if(!result) {
+      throw HttpError(404, "Not found");
+    }
+
     res.json(result);
 
   } catch (error) {
