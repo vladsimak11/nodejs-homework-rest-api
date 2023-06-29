ba89ffc836e20ea012c46360564f4ac718f29437
Vladyslav Simak <simakv21@gmail.com>
2023-06-29 18:28:39 +0300

    Fix path and registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,8 +23,10 @@ const register = async(req, res, next) => {
     const newUser = await User.create({...req.body, password: hashPassword });
   
     res.status(201).json({
-      email: newUser.email,
-      subscription: newUser.subscription
+      user: {
+        email: newUser .email,
+        subscription: newUser .subscription
+      }
     });
   } catch (error) {
     next(error);
--- validator/users.js
+++ validator/users.js
@@ -3,10 +3,12 @@ const Joi = require("joi");
 const registerSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
+  subscription: Joi.string(),
 });
 
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
   password: Joi.string().min(6).required(),
 });
 
