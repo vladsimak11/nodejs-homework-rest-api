8b66aa3fc4e757b77f1ef3dab54007f799ab0047
Vladyslav <93095221+vladsimak11@users.noreply.github.com>
2023-07-01 13:10:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,28 +15,4 @@ mongoose.connect(DB_HOST)
   .catch( error => {
     console.log(error.message);
     process.exit(1);
-  })
-
-
-
-  const updateAvatar = async (req, res) => {
-    const { _id } = req.user;
-    const { path: tempUpload, originalname } = req.file;
-    const filename = `${_id}_${originalname}`;
-  
-    const resultUpload = path.join(avatarsDir, filename);
-  
-    Jimp.read(tempUpload, (err, img) => {
-      if (err) throw err;
-      img.resize(250, 250).write(resultUpload);
-    });
-  
-    await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("avatars", filename);
-  
-    await User.findByIdAndUpdate(_id, { avatarURL });
-  
-    res.json({
-      avatarURL,
-    });
-  };
+  });
